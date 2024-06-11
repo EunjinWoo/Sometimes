7808f48c22e747dc7d6552fbb86cd006d9098b55
JiwonShin <dev.jiwonshin@gmail.com>
2024-06-11 20:25:53 +0900

    jiwon commit

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,31 +1,571 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const getTodo = /* GraphQL */ `
-  query GetTodo($id: ID!) {
-    getTodo(id: $id) {
+export const getUser = /* GraphQL */ `
+  query GetUser($id: ID!) {
+    getUser(id: $id) {
       id
       name
       description
-      description2
+      emojiPath
+      profilePath
+      gender
       createdAt
       updatedAt
       __typename
     }
   }
 `;
-export const listTodos = /* GraphQL */ `
-  query ListTodos(
-    $filter: ModelTodoFilterInput
+export const listUsers = /* GraphQL */ `
+  query ListUsers(
+    $filter: ModelUserFilterInput
     $limit: Int
     $nextToken: String
   ) {
-    listTodos(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    listUsers(filter: $filter, limit: $limit, nextToken: $nextToken) {
       items {
         id
         name
         description
-        description2
+        emojiPath
+        profilePath
+        gender
+        createdAt
+        updatedAt
+        __typename
+      }
+      nextToken
+      __typename
+    }
+  }
+`;
+export const getLike = /* GraphQL */ `
+  query GetLike($id: ID!) {
+    getLike(id: $id) {
+      id
+      likeUserId
+      likedUserId
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const listLikes = /* GraphQL */ `
+  query ListLikes(
+    $filter: ModelLikeFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listLikes(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        likeUserId
+        likedUserId
+        createdAt
+        updatedAt
+        __typename
+      }
+      nextToken
+      __typename
+    }
+  }
+`;
+export const getStep = /* GraphQL */ `
+  query GetStep($id: ID!) {
+    getStep(id: $id) {
+      id
+      requestUserId
+      targetUserId
+      status
+      isAllowed
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const listSteps = /* GraphQL */ `
+  query ListSteps(
+    $filter: ModelStepFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listSteps(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        requestUserId
+        targetUserId
+        status
+        isAllowed
+        createdAt
+        updatedAt
+        __typename
+      }
+      nextToken
+      __typename
+    }
+  }
+`;
+export const getQuestion = /* GraphQL */ `
+  query GetQuestion($id: ID!) {
+    getQuestion(id: $id) {
+      id
+      kind
+      description
+      type
+      isRequired
+      userId
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const listQuestions = /* GraphQL */ `
+  query ListQuestions(
+    $filter: ModelQuestionFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listQuestions(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        kind
+        description
+        type
+        isRequired
+        userId
+        createdAt
+        updatedAt
+        __typename
+      }
+      nextToken
+      __typename
+    }
+  }
+`;
+export const getLocation = /* GraphQL */ `
+  query GetLocation($id: ID!) {
+    getLocation(id: $id) {
+      id
+      x
+      y
+      userId
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const listLocations = /* GraphQL */ `
+  query ListLocations(
+    $filter: ModelLocationFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listLocations(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        x
+        y
+        userId
+        createdAt
+        updatedAt
+        __typename
+      }
+      nextToken
+      __typename
+    }
+  }
+`;
+export const getChatRoom = /* GraphQL */ `
+  query GetChatRoom($id: ID!) {
+    getChatRoom(id: $id) {
+      id
+      participant1Id
+      participant2Id
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const listChatRooms = /* GraphQL */ `
+  query ListChatRooms(
+    $filter: ModelChatRoomFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listChatRooms(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        participant1Id
+        participant2Id
+        createdAt
+        updatedAt
+        __typename
+      }
+      nextToken
+      __typename
+    }
+  }
+`;
+export const getChatItem = /* GraphQL */ `
+  query GetChatItem($id: ID!) {
+    getChatItem(id: $id) {
+      id
+      message
+      senderId
+      chatroomId
+      action
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const listChatItems = /* GraphQL */ `
+  query ListChatItems(
+    $filter: ModelChatItemFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listChatItems(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        message
+        senderId
+        chatroomId
+        action
+        createdAt
+        updatedAt
+        __typename
+      }
+      nextToken
+      __typename
+    }
+  }
+`;
+export const getNotification = /* GraphQL */ `
+  query GetNotification($id: ID!) {
+    getNotification(id: $id) {
+      id
+      receiverId
+      senderId
+      message
+      type
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const listNotifications = /* GraphQL */ `
+  query ListNotifications(
+    $filter: ModelNotificationFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listNotifications(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        receiverId
+        senderId
+        message
+        type
+        createdAt
+        updatedAt
+        __typename
+      }
+      nextToken
+      __typename
+    }
+  }
+`;
+export const getLikeByLikeUserId = /* GraphQL */ `
+  query GetLikeByLikeUserId(
+    $likeUserId: ID!
+    $sortDirection: ModelSortDirection
+    $filter: ModelLikeFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    getLikeByLikeUserId(
+      likeUserId: $likeUserId
+      sortDirection: $sortDirection
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+    ) {
+      items {
+        id
+        likeUserId
+        likedUserId
+        createdAt
+        updatedAt
+        __typename
+      }
+      nextToken
+      __typename
+    }
+  }
+`;
+export const getLikeByLikedUserId = /* GraphQL */ `
+  query GetLikeByLikedUserId(
+    $likedUserId: ID!
+    $sortDirection: ModelSortDirection
+    $filter: ModelLikeFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    getLikeByLikedUserId(
+      likedUserId: $likedUserId
+      sortDirection: $sortDirection
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+    ) {
+      items {
+        id
+        likeUserId
+        likedUserId
+        createdAt
+        updatedAt
+        __typename
+      }
+      nextToken
+      __typename
+    }
+  }
+`;
+export const getStepByRequestUserId = /* GraphQL */ `
+  query GetStepByRequestUserId(
+    $requestUserId: ID!
+    $sortDirection: ModelSortDirection
+    $filter: ModelStepFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    getStepByRequestUserId(
+      requestUserId: $requestUserId
+      sortDirection: $sortDirection
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+    ) {
+      items {
+        id
+        requestUserId
+        targetUserId
+        status
+        isAllowed
+        createdAt
+        updatedAt
+        __typename
+      }
+      nextToken
+      __typename
+    }
+  }
+`;
+export const getStepByTargetUserId = /* GraphQL */ `
+  query GetStepByTargetUserId(
+    $targetUserId: ID!
+    $sortDirection: ModelSortDirection
+    $filter: ModelStepFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    getStepByTargetUserId(
+      targetUserId: $targetUserId
+      sortDirection: $sortDirection
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+    ) {
+      items {
+        id
+        requestUserId
+        targetUserId
+        status
+        isAllowed
+        createdAt
+        updatedAt
+        __typename
+      }
+      nextToken
+      __typename
+    }
+  }
+`;
+export const getQuestionByUserId = /* GraphQL */ `
+  query GetQuestionByUserId(
+    $userId: ID!
+    $sortDirection: ModelSortDirection
+    $filter: ModelQuestionFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    getQuestionByUserId(
+      userId: $userId
+      sortDirection: $sortDirection
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+    ) {
+      items {
+        id
+        kind
+        description
+        type
+        isRequired
+        userId
+        createdAt
+        updatedAt
+        __typename
+      }
+      nextToken
+      __typename
+    }
+  }
+`;
+export const getLocationByUserId = /* GraphQL */ `
+  query GetLocationByUserId(
+    $userId: ID!
+    $sortDirection: ModelSortDirection
+    $filter: ModelLocationFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    getLocationByUserId(
+      userId: $userId
+      sortDirection: $sortDirection
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+    ) {
+      items {
+        id
+        x
+        y
+        userId
+        createdAt
+        updatedAt
+        __typename
+      }
+      nextToken
+      __typename
+    }
+  }
+`;
+export const getChatRoomByParticipant1Id = /* GraphQL */ `
+  query GetChatRoomByParticipant1Id(
+    $participant1Id: ID!
+    $sortDirection: ModelSortDirection
+    $filter: ModelChatRoomFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    getChatRoomByParticipant1Id(
+      participant1Id: $participant1Id
+      sortDirection: $sortDirection
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+    ) {
+      items {
+        id
+        participant1Id
+        participant2Id
+        createdAt
+        updatedAt
+        __typename
+      }
+      nextToken
+      __typename
+    }
+  }
+`;
+export const getChatRoomByParticipant2Id = /* GraphQL */ `
+  query GetChatRoomByParticipant2Id(
+    $participant2Id: ID!
+    $sortDirection: ModelSortDirection
+    $filter: ModelChatRoomFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    getChatRoomByParticipant2Id(
+      participant2Id: $participant2Id
+      sortDirection: $sortDirection
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+    ) {
+      items {
+        id
+        participant1Id
+        participant2Id
+        createdAt
+        updatedAt
+        __typename
+      }
+      nextToken
+      __typename
+    }
+  }
+`;
+export const getNotificationByReceiverId = /* GraphQL */ `
+  query GetNotificationByReceiverId(
+    $receiverId: ID!
+    $sortDirection: ModelSortDirection
+    $filter: ModelNotificationFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    getNotificationByReceiverId(
+      receiverId: $receiverId
+      sortDirection: $sortDirection
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+    ) {
+      items {
+        id
+        receiverId
+        senderId
+        message
+        type
+        createdAt
+        updatedAt
+        __typename
+      }
+      nextToken
+      __typename
+    }
+  }
+`;
+export const getNotificationBySenderId = /* GraphQL */ `
+  query GetNotificationBySenderId(
+    $senderId: ID!
+    $sortDirection: ModelSortDirection
+    $filter: ModelNotificationFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    getNotificationBySenderId(
+      senderId: $senderId
+      sortDirection: $sortDirection
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+    ) {
+      items {
+        id
+        receiverId
+        senderId
+        message
+        type
         createdAt
         updatedAt
         __typename
